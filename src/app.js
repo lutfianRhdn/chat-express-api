caba51df321e8c5de1316172e944fead924ba5b9
deweks0 <lutfianjr1029f@gmail.com>
2021-07-11 19:19:23 +0700

    added morgan

--- src/app.js
+++ src/app.js
@@ -2,9 +2,12 @@ const express = require("express");
 const app = express();
 const route = require("./api/routes/index");
 const bodyParser = require("body-parser");
+const morgan = require("morgan");
+
 require("dotenv").config();
 require("./config/database");
 
+app.use(morgan);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use("/api", route);
